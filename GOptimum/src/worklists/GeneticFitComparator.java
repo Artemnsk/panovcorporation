638a30b1c825c1dbcb58405f0cbcec72b1f12a45
artemnsk <shelkov1991@gmail.com>
2014-06-02 22:33:20 +0700

    compare of GeneticFitComparator can distinguish every box now

--- GOptimum/src/worklists/GeneticFitComparator.java
+++ GOptimum/src/worklists/GeneticFitComparator.java
@@ -14,17 +14,37 @@ public class GeneticFitComparator implements Comparator<Box> {
 	}
 	@Override
 	public int compare(Box b1, Box b2) {
-		assert(b1.getDimension() == b2.getDimension());
-		if (b1 == b2) return 0;
-		double fit1 = iGA.calcFitFunction(b1);
-		double fit2 = iGA.calcFitFunction(b2);
-		//if (fit1 == fit2)
-			//return ( new AscendingLowBoundBoxComparator() ).compare(b1, b2);
-		//in "=" case we MUST NOT switch (+1) or we will jump forever...
-		if (fit1 < fit2)
-			return +1;
-		else
-			return -1;
+        assert(b1.getDimension() == b2.getDimension());
+        if (b1 == b2)
+            return 0;
+        double fit1 = iGA.calcFitFunction(b1);
+        double fit2 = iGA.calcFitFunction(b2);
+        if (fit1 == fit2) {
+            if (b1.equals(b2))
+                return 0;
+            double hi1 = b1.getFunctionValue().hi();
+            double hi2 = b2.getFunctionValue().hi();
+            if (hi1 == hi2) {
+                double sumWid1 = 0, sumWid2 = 0;
+                for (int i = b1.getDimension()-1; i >= 0; i--) {
+                    sumWid1 += b1.getInterval(i).wid();
+                    sumWid2 += b2.getInterval(i).wid();
+                }
+                if (sumWid1 == sumWid2) { // ok. everything is absolutely equals, but these boxes are different. lets distinguish them somehow
+                    for (int i = b1.getDimension()-1; i >= 0; i--) {
+                        if (b1.getInterval(i).lo() < b2.getInterval(i).lo() ||
+                                b1.getInterval(i).hi() < b2.getInterval(i).hi() )
+                            return -1;
+                        if (b1.getInterval(i).lo() > b2.getInterval(i).lo() ||
+                                b1.getInterval(i).hi() > b2.getInterval(i).hi() )
+                            return 1;
+                    }
+                } else
+                    return sumWid1 > sumWid2 ? -1 : 1; // wider boxes goes first
+            } else
+                return hi1 > hi2 ? -1 : 1; // offer boxes with wider function estimation first
+        }
+        return (fit1 > fit2) ? +1 : -1;
 	}
 
 }
